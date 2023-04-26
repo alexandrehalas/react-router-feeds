e1890b08442cf4f97c1a86c6fa671f8a83dc3001
Alexandre Halas <alexandrehalas@gmail.com>
2023-04-26 17:30:05 -0300

    Criando o roteador e rotas iniciais

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./paginas/About";
+import Home from "./paginas/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<h1>Página não encontrada</h1>} />
+      </Routes>
     </BrowserRouter>
   )
 }
