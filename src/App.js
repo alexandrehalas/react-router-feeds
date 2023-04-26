31db68c52e6cfb7ebc048ea8dee794c51769030d
Alexandre Halas <alexandrehalas@gmail.com>
2023-04-25 22:12:38 -0300

    Alternando rotas com javascript

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import About from "./paginas/About";
+import Home from "./paginas/Home";
+
+console.log(window.location)
+
+const pagina = window.location.pathname === '/' ? <Home /> : <About />
+
 export default function App() {
-  return (
-    <h1>React Router Feeds</h1>
-  );
+  return pagina;
 }
