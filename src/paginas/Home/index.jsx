db4da51129c1a6ef7dc9ca92988556fe765b2df3
Alexandre Halas <alexandrehalas@gmail.com>
2023-04-26 20:43:25 -0300

    Adicionado componente Post na página Home

--- src/paginas/Home/index.jsx
+++ src/paginas/Home/index.jsx
@@ -1,10 +1,20 @@
-import Banner from '../../componentes/Banner'
+import Banner from 'componentes/Banner'
+import styles from './Home.module.css'
+
+import Post from 'componentes/Post'
+import posts from 'json/posts.json'
 
 export default function Home() {
     return (
         <main>
             <Banner />
-            <h1>Home</h1>
+            <ul className={styles.posts}>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Post post={post} />
+                    </li>
+                ))}
+            </ul>
         </main>
     )
 }
