bca4b300aa984f6a9f0ccab2024f39d9f5ff620e
Alexandre Halas <alexandrehalas@gmail.com>
2023-04-29 20:19:45 -0300

    Incluído posts recomendados na página do Post

--- src/paginas/Post/index.jsx
+++ src/paginas/Post/index.jsx
@@ -1,10 +1,12 @@
 import PageDefault from 'componentes/PageDefault'
+import PostCard from 'componentes/PostCard'
 import PostTemplate from 'componentes/PostTemplate'
 import posts from 'json/posts.json'
 import NotFound from 'paginas/NotFound'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 import { Route, Routes, useParams } from "react-router-dom"
 import './Post.css'
+import styles from './Post.module.css'
 
 export default function Post() {
 
@@ -14,6 +16,11 @@ export default function Post() {
     if (!post) {
         return <NotFound />
     }
+
+    const recommendedPosts = posts
+        .filter((post) => post.id !== Number(params.id))
+        .sort((a, b) => b.id - a.id)
+        .slice(0, 4)
 
     return (
         <Routes>
@@ -28,6 +35,14 @@ export default function Post() {
                                 {post.text}
                             </ReactMarkdown>
                         </div>
+                        <h2 className={styles.title}>Outros posts que você pode gostar:</h2>
+                        <ul className={styles.recommendedPosts}>
+                            {recommendedPosts.map((recommendedPost) => {
+                                return <li key={recommendedPost.id}>
+                                    <PostCard post={recommendedPost} />
+                                </li>
+                            })}
+                        </ul>
                     </PostTemplate>
                 }></Route>
             </Route>
