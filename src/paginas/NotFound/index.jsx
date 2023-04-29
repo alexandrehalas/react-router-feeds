f8932a0b04c4908294a23809939d6b7a23b7c9f5
Alexandre Halas <alexandrehalas@gmail.com>
2023-04-29 15:22:27 -0300

    Finalizando tela NotFound

--- src/paginas/NotFound/index.jsx
+++ src/paginas/NotFound/index.jsx
@@ -1,7 +1,10 @@
-import styles from './NotFound.module.css'
-import error404 from './error404.png'
+import Button from 'componentes/Button';
+import { useNavigate } from 'react-router-dom';
+import styles from './NotFound.module.css';
+import error404 from './error404.png';
 
 export default function NotFound() {
+    const navigate = useNavigate();
     return (
         <>
             <div className={styles.contentContainer}>
@@ -9,8 +12,8 @@ export default function NotFound() {
                 <h1 className={styles.title}>Ops! Página não encontrada.</h1>
                 <p className={styles.paragraph}>Tem certeza de que era isso que você estava procurando?</p>
                 <p className={styles.paragraph}>Aguarde uns instantes e recarregue a página</p>
-                <div className={styles.buttonContainer}>
-                    <button>Voltar</button>
+                <div className={styles.buttonContainer} onClick={() => navigate(-1)}>
+                    <Button size='lg'>Voltar</Button>
                 </div>
                 <img className={styles.dogImage} src={error404} alt='Cachorro de óculos com roupas de humano'></img>
             </div>
