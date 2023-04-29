d8ea0b4f161fcf5121168b16782d7b6e31c6a7f1
Alexandre Halas <alexandrehalas@gmail.com>
2023-04-29 15:44:17 -0300

    Diminuído parágrafo do Banner

--- src/componentes/Banner/index.jsx
+++ src/componentes/Banner/index.jsx
@@ -9,7 +9,7 @@ export default function Banner() {
                     Lorem Ipsum
                 </h1>
                 <p className={styles.paragraph}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id porta nibh venenatis cras sed felis eget. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna. Turpis egestas sed tempus urna. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Massa sed elementum tempus egestas sed sed risus pretium quam. Cursus metus aliquam eleifend mi in nulla posuere sollicitudin.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id porta nibh venenatis cras sed felis eget. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna. Turpis egestas sed tempus urna. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit.
                 </p>
             </div>
             <div className={styles.images}>
