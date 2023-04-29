369a252855be5513cffefef127ee0b33546bd65a
Alexandre Halas <alexandrehalas@gmail.com>
2023-04-29 15:47:02 -0300

    Ajuste de rotas descendentes

--- src/routes.js
+++ src/routes.js
@@ -16,7 +16,7 @@ export default function AppRoutes() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
         </Route>
-        <Route path="posts/:id" element={<Post />} />
+        <Route path="posts/:id/*" element={<Post />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
